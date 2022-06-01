43cad5fde8c0b403d5da4e8a3f2d3aad563b1192
Arullzzz <106241765+Arullzzz@users.noreply.github.com>
2022-06-01 13:54:47 +0800

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -1,6 +1,8 @@
 let handler = async m => m.reply(`
 Bot Ini Mengunakan Sc : 
 https://github.com/Johannes2803/Mythia-Batford
+Sc saya:
+https://github.com/Arullzzz/Mythia-Batford
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['sc']
 handler.tags = ['info']
